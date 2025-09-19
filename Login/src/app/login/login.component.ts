44d41213793f5d8c530724baf0a2e3788fff4664
Mathias Toala <fetoala@outlook.com>
2025-09-18 23:26:06 -0500

    add angular materials to elaborate better UI

--- Login/src/app/login/login.component.ts
+++ Login/src/app/login/login.component.ts
@@ -1,8 +1,49 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatSortModule} from '@angular/material/sort';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatTableModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatSortModule,
+    MatExpansionModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
